dc91411cb0b65f602e4a067eaf71e4f567b4c2a1
Oliver <ottmar.hittzfeld@web.de>
2025-07-11 18:44:28 +0200

    Remove environment setup from plugin

--- ibtool.py
+++ ibtool.py
@@ -24,22 +24,7 @@ of building footprints
 """
 
 import os
-import sys
 import time
-
-# Constants for configuration
-PYTHONPATH = '/helpers'
-
-def initialize_environment():
-    """Set up environment variables and system paths."""
-    os.environ['PYTHONPATH'] = PYTHONPATH
-    sys.path.append(PYTHONPATH)
-    os.environ['PATH'] += r";C:\Program Files\QGIS 3.40.0\bin;"
-    os.environ['PYTHONPATH'] = r"C:\Program Files\QGIS 3.40.0\apps\qgis\python"
-    sys.path.append(r"C:\Program Files\QGIS 3.40.0\apps\qgis\python")
-
-# Initialize the environment
-initialize_environment()
 
 from qgis.PyQt.QtCore import (
     QCoreApplication,
