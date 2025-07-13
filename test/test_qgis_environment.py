c2ad4b862ac20d2a1ec131e204fb1447b51b26a2
Oliver <ottmar.hittzfeld@web.de>
2025-07-13 21:48:02 +0200

    Update test module docstring to specify supported QGIS versions (3.40-3.50)

--- test/test_qgis_environment.py
+++ test/test_qgis_environment.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 """Tests for QGIS functionality.
 
+Supported QGIS versions: 3.40-3.50.
 
 .. note:: This program is free software; you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published by
