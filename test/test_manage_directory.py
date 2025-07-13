08d452080a3391e85bf82af87ca7b048bb1eca7d
Oliver <ottmar.hittzfeld@web.de>
2025-07-13 21:47:52 +0200

    Update system_utils.py to specify supported QGIS versions (3.40-3.50)

--- test/test_manage_directory.py
+++ test/test_manage_directory.py
@@ -3,6 +3,12 @@ import tempfile
 import unittest
 from unittest.mock import patch
 
+# Nutze die zentrale QGIS-Initialisierung aus test/__init__.py
+from .utilities import get_qgis_app
+
+QGIS_APP = get_qgis_app()
+
+
 class DummyLogger:
     logs = []
 
@@ -12,23 +18,44 @@ class DummyLogger:
 
 
 class ManageDirectoryTest(unittest.TestCase):
+    EXPECTED_DIRECTORY_NAME = 'IB_Tool_Results'
+
     def setUp(self):
         self.tmp = tempfile.TemporaryDirectory()
-        from helpers import system_utils as su
-        self.su = su
-        self.patcher = patch.object(self.su, 'Logger', DummyLogger)
+        self._setup_manage_directory_function()
+        self._setup_logger_mock()
+
+    def _setup_manage_directory_function(self):
+        """Importiert und setzt die manage_directory Funktion."""
+        from ..helpers.system_utils import manage_directory
+        self.manage_directory = manage_directory
+
+    def _setup_logger_mock(self):
+        """Richtet das Logger-Mocking ein."""
+        from ..helpers import system_utils
+        self.patcher = patch.object(system_utils, 'Logger', DummyLogger)
         self.patcher.start()
+
+    def _create_test_workspace(self):
+        """Erstellt einen Test-Workspace und gibt den Pfad zurück."""
+        workspace_path = os.path.join(self.tmp.name, 'ws')
+        os.makedirs(workspace_path)
+        return workspace_path
 
     def tearDown(self):
         self.patcher.stop()
         self.tmp.cleanup()
 
-    def test_manage_directory_creates_folder_posix(self):
-        workspace = os.path.join(self.tmp.name, 'ws')
-        os.makedirs(workspace)
-        self.su.manage_directory(workspace, del_part_log=False)
-        expected = os.path.join(workspace, 'IB_Tool_Results')
-        self.assertTrue(os.path.isdir(expected))
+    def test_manage_directory_creates_expected_folder(self):
+        """Testet, dass manage_directory den erwarteten Ordner erstellt."""
+        workspace_path = self._create_test_workspace()
+
+        self.manage_directory(workspace_path, del_part_log=False)
+
+        expected_directory = os.path.join(workspace_path,
+                                          self.EXPECTED_DIRECTORY_NAME)
+        self.assertTrue(os.path.isdir(expected_directory))
+
 
 if __name__ == '__main__':
     unittest.main()
