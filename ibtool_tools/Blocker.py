ee9f969a05a8177b109d691bd497755086cceb5c
Oliver <ottmar.hittzfeld@web.de>
2025-07-16 21:12:49 +0200

    Standardize code style and formatting in `Blocker.py`

--- ibtool_tools/Blocker.py
+++ ibtool_tools/Blocker.py
@@ -7,40 +7,40 @@ from ..helpers.logger import Logger
 
 
 def blocker(strassen, hu_input, partition):
-
     """
+    Create city blocks from partition outline and road network.
+
     :param strassen: road network as polyline (QGIS layer)
     :param hu_input: building footprints as polygon (QGIS layer)
-    :param partition: part of study area for that city block is calculated (QGIS layer)
+    :param partition: part of study area for that city block is calculated
+                     (QGIS layer)
     :return: city block (QGIS layer)
-
-    - create city blocks of partition outline and road network
     """
 
     # Create partition outline as lines
     partition_outline = processing.run("native:polygonstolines", {
         'INPUT': partition,
         'OUTPUT': 'TEMPORARY_OUTPUT'
-        })['OUTPUT']
+    })['OUTPUT']
 
     # Intersect roads with partition
     strassen_intersect = processing.run("native:intersection", {
         'INPUT': strassen,
         'OVERLAY': partition,
-        'OUTPUT':'TEMPORARY_OUTPUT'
-        })['OUTPUT']
+        'OUTPUT': 'TEMPORARY_OUTPUT'
+    })['OUTPUT']
 
     # Merge outlines and road intersections
     strassen_merge = processing.run("native:mergevectorlayers", {
         'LAYERS': [partition_outline, strassen_intersect],
-        'OUTPUT':'TEMPORARY_OUTPUT'
-        })['OUTPUT']
+        'OUTPUT': 'TEMPORARY_OUTPUT'
+    })['OUTPUT']
 
     blocks_layer = processing.run("native:polygonize", {
-        'INPUT':strassen_merge,
-        'KEEP_FIELDS':False,
-        'OUTPUT':'TEMPORARY_OUTPUT'
-        })['OUTPUT']
+        'INPUT': strassen_merge,
+        'KEEP_FIELDS': False,
+        'OUTPUT': 'TEMPORARY_OUTPUT'
+    })['OUTPUT']
 
     # Select blocks containing buildings
     processing.run("native:selectbylocation", {
@@ -61,7 +61,9 @@ def blocker(strassen, hu_input, partition):
     # Add NAME field
     blocks_layer.startEditing()
     if not blocks_layer.dataProvider().fieldNameIndex("NAME") >= 0:
-        blocks_layer.dataProvider().addAttributes([QgsField("NAME", QMetaType.QString)])
+        blocks_layer.dataProvider().addAttributes([
+            QgsField("NAME", QMetaType.QString)
+        ])
         blocks_layer.updateFields()
 
     # Calculate NAME field values
